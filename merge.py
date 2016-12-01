bf65c7214bab5e9643a40b45440220e69a2e93e2
LechuckRoh <member@lechuck2011.cafe24.com>
2016-12-01 23:31:22 +0900

    [ADD] copy/move mp3 files

--- merge.py
+++ merge.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import os
 import re
+import shutil
 
 
 # get filenames with matching pattern in given path
@@ -10,8 +11,9 @@ def get_filenames(path, pattern):
     return glob.glob(pattern)
 
 
-def parse_filename(filename):
-    search = re.search('(^\d+)\.?(.*)-(.*).mp3', filename, re.IGNORECASE)
+# parse filename and return dictionary
+def parse_filename(name):
+    search = re.search('(^\d+)\.?(.*)-(.*).mp3', name, re.IGNORECASE)
     if search:
         return {
             'rank': search.group(1),
@@ -19,18 +21,33 @@ def parse_filename(filename):
             'title': search.group(3).strip()
         }
     else:
-        return {'title': filename}
+        return {'title': name}
+
+
+def copy_mp3_files(src, dest, move):
+    for filename in get_filenames(src, "*.mp3"):
+        info = parse_filename(filename)
+        if info['singer']:
+            new_filename = "%s - %s.mp3" % (info['singer'], info['title'])
+        else:
+            new_filename = filename
+        src_path = os.path.join(src, filename)
+        dest_path = os.path.join(dest, new_filename)
+        if move:
+            print("[MOVE] %s => %s" % (src_path, dest_path))
+            shutil.move(src_path, dest_path)
+        else:
+            print("[COPY] %s => %s" % (src_path, dest_path))
+            shutil.copyfile(src_path, dest_path)
 
 
 parser = argparse.ArgumentParser(description='Move mp3 files to another directory')
 parser.add_argument('--src', help='source directory', default='./')
 parser.add_argument('--dest', help='destination directory', required=True)
+parser.add_argument('--move', help='move file', action='store_true')
 args = parser.parse_args()
 
-print("[MOVE] %s -> %s" % (args.src, args.dest))
+print("source dir : %s" % args.src)
+print("destination dir : %s" % args.dest)
 
-files = get_filenames(args.src, "*.mp3")
-
-for f in files:
-    info = parse_filename(f)
-    print("%s => %s" % (f, info))
+copy_mp3_files(os.path.abspath(args.src), os.path.abspath(args.dest), args.move)
