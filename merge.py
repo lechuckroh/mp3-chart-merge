74f62e0e37b53a5aa99aa290836b2951f7f9cad6
lechuck <lechuck@kstec.co.kr>
2016-12-02 10:59:58 +0900

    [ADD] find duplicate files by normailzing filename

--- merge.py
+++ merge.py
@@ -24,22 +24,43 @@ def parse_filename(name):
         return {'title': name}
 
 
+# Remove ranking from filename
+def remove_ranking(name):
+    info = parse_filename(name)
+    if info['singer']:
+        return "%s - %s.mp3" % (info['singer'], info['title'])
+    else:
+        return name
+
+
+# Normalize string
+# Removes whitespace, special characters
+def normailze(name):
+    return re.sub('[^A-Za-z0-9가-힣]', '', name)
+
+
+# Copy or move mp3 files from src to dest directory
 def copy_mp3_files(src, dest, move):
+    dest_filenames = get_filenames(dest, "*.mp3")
+    dest_normalized_filenames = map(lambda x: normailze(x), dest_filenames)
+
     for filename in get_filenames(src, "*.mp3"):
-        info = parse_filename(filename)
-        if info['singer']:
-            new_filename = "%s - %s.mp3" % (info['singer'], info['title'])
-        else:
-            new_filename = filename
+        new_filename = remove_ranking(filename)
+        normalized = normailze(new_filename)
         src_path = os.path.join(src, filename)
+
+        # skip copy/move if exists
+        if normalized in dest_normalized_filenames:
+            print("[SKIP] %s" % filename)
+            continue
+
+        # copy/move
         dest_path = os.path.join(dest, new_filename)
+        print("[%s] %s => %s" % ("MOVE" if move else "COPY", filename, new_filename))
         if move:
-            print("[MOVE] %s => %s" % (src_path, dest_path))
             shutil.move(src_path, dest_path)
         else:
-            print("[COPY] %s => %s" % (src_path, dest_path))
             shutil.copyfile(src_path, dest_path)
-
 
 parser = argparse.ArgumentParser(description='Move mp3 files to another directory')
 parser.add_argument('--src', help='source directory', default='./')
